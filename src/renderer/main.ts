588d444fa2be2d0b3a4d86c99f481987fc3fa031
Koki Fukuda <ko.fu.dev@gmail.com>
2022-03-30 00:47:10 +0900

    Make Gopher to walk

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,9 +3,40 @@ for (const [i, f] of gopherFrames.entries()) {
     f.src = `out0${i + 1}.png`;
 }
 
+class GopherAnimator {
+    frameNum = 0;
+    frameReqId: number|undefined = undefined;
+
+    private requestNextFrame = () => {
+        this.frameReqId = requestAnimationFrame(this.draw);
+    };
+
+    start = () => {
+        this.requestNextFrame();
+    };
+
+    stop = () => {
+        if (typeof this.frameReqId !== 'undefined') {
+            cancelAnimationFrame(this.frameReqId);
+            this.frameReqId = undefined;
+        }
+    };
+
+    draw = () => {
+        this.requestNextFrame();
+
+        const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+        const ctx = canvas.getContext('2d')!!;
+        ctx.clearRect(0, 0, 200, 200);
+        ctx.drawImage(gopherFrames[Math.sin(this.frameNum / 30 * Math.PI) * 2.3 >> 0], 0, 0);
+        this.frameNum++;
+        if (this.frameNum >= 30) {
+            this.frameNum = 0;
+        }
+    };
+}
+
 addEventListener('load', () => {
-    const canvas = <HTMLCanvasElement>document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')!!;
-    console.log(gopherFrames[0]);
-    ctx.drawImage(gopherFrames[0], 0, 0);
+    const animator = new GopherAnimator;
+    animator.start();
 });
