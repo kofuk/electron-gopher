b42e64ad5a35fca2375c67c38eb04b3bb9a3b4cc
Koki Fukuda <ko.fu.dev@gmail.com>
2022-03-30 00:21:11 +0900

    Draw image on Canvas

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,1 +1,11 @@
-console.log('hoge');
+const gopherFrames = [new Image, new Image, new Image];
+for (const [i, f] of gopherFrames.entries()) {
+    f.src = `out0${i + 1}.png`;
+}
+
+addEventListener('load', () => {
+    const canvas = <HTMLCanvasElement>document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')!!;
+    console.log(gopherFrames[0]);
+    ctx.drawImage(gopherFrames[0], 0, 0);
+});
